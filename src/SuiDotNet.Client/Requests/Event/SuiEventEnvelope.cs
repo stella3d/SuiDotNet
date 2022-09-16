47328b4d8ab40b5d7d62f024227cdd586ead8b93
Stella Cannefax <stella@mystenlabs.com>
2022-09-16 03:27:27 -0500

    add new event envelope types

--- src/SuiDotNet.Client/Requests/Event/SuiEventEnvelope.cs
+++ src/SuiDotNet.Client/Requests/Event/SuiEventEnvelope.cs
@@ -4,24 +4,46 @@ using Newtonsoft.Json;
 namespace SuiDotNet.Client.Requests
 {
     [Serializable]
-    public class SuiEventEnvelope
+    public class BaseEventEnvelope<TTime, TEvent>
     {
         [JsonProperty("timestamp")]
-        public DateTimeOffset Timestamp { get; }
+        public TTime Timestamp { get; }
         
         [JsonProperty("txDigest")]
         public string TxDigest { get; }
         
         [JsonProperty("event")]
-        public object Event { get; }
+        public TEvent Event { get; }
 
         // 'event' is reserved, so disable this warning
-        // ReSharper disable once InconsistentNaming
-        public SuiEventEnvelope(ulong time, string digest, object event_)
+        // ReSharper disable InconsistentNaming
+        public BaseEventEnvelope(TTime time, string digest, TEvent event_)
         {
-            Timestamp = DateTimeOffset.FromUnixTimeMilliseconds((long) time);
+            Timestamp = time;
             TxDigest = digest;
             Event = event_;
         }
     }
+    
+    [Serializable]
+    public class SuiEventEnvelope: BaseEventEnvelope<DateTimeOffset, object>
+    {
+        public SuiEventEnvelope(DateTimeOffset time, string digest, object event_)
+            : base(time, digest, event_) { }
+
+        public SuiEventEnvelope(RawSuiEventEnvelope raw)
+            : base(
+                DateTimeOffset.FromUnixTimeMilliseconds((long) raw.Timestamp),
+                raw.TxDigest,
+                raw.Event) { }
+    }
+    
+    [Serializable]
+    public class RawSuiEventEnvelope: BaseEventEnvelope<ulong, object>
+    {
+        public RawSuiEventEnvelope(ulong time, string digest, object event_)
+            : base(time, digest, event_) { }
+    }
+    
+    // ReSharper enable InconsistentNaming
 }
