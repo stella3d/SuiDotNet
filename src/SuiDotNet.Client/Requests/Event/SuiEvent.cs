57deb805226925a916f92be72a2991a591711b92
Stella Cannefax <stella@mystenlabs.com>
2022-09-16 16:26:47 -0500

    Create SuiEvent.cs

---
+++ src/SuiDotNet.Client/Requests/Event/SuiEvent.cs
@@ -0,0 +1,17 @@
+using System;
+using Newtonsoft.Json;
+
+namespace SuiDotNet.Client.Requests
+{
+    [Serializable]
+    public class SuiEvent<T>
+    {
+        [JsonProperty("moveEvent")] public MoveEvent<T>? MoveEvent { get; set; }
+        [JsonProperty("publish")] public PublishEvent? Publish { get; set; }
+        [JsonProperty("transferObject")] public TransferObjectEvent? TransferObject { get; set; }
+        [JsonProperty("deleteObject")] public DeleteObjectEvent? DeleteObject { get; set; }
+        [JsonProperty("newObject")] public NewObjectEvent? NewObject { get; set; }
+        [JsonProperty("epochChange")] public object? EpochChange { get; set; }
+        [JsonProperty("checkpoint")] public ulong? Checkpoint { get; set; }
+    }
+}
